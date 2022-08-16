ba7ae0864dd6aef002bdcd95561be4d3d17a6d49
Dan Carpenter <dcarpenter@microsoft.com>
2022-08-16 10:12:08 -0600

    az extension updates

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,5 +9,16 @@ echo "$(date)    post-create start" >> "$HOME/status"
 git -C /workspaces/imdb-app pull
 git -C /workspaces/webvalidate pull
 
+
+echo "adding az cli extensions"
+az extension add --name connectedk8s
+az extension add -n k8s-extension
+az extension add --name k8s-configuration
+
+export GITHUB_TOKEN="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"
+
+
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
+
+
